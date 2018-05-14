e9176f39daccf86d91988f1c320b2a6090af4c89
dbrud951 <dbrud951@hanmail.net>
2018-05-14 19:35:36 +0900

    정수 두 개 입력 받아서 합을 출력하는 프로그램 작성(C++)

---
+++ C++/Level1_2/Level1_2/Level1_2.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+
+using std::cin;
+using std::cout;
+
+int main(void) {
+	int A, B, sum=0;
+
+	cin >> A >> B;
+	sum = A + B;
+	cout << sum;
+}
---
+++ JAVA/Level_1/src/Level_1_2/Level_1_2.java
@@ -0,0 +1,17 @@
+package Level_1_2;
+
+import java.util.Scanner;
+
+public class Level_1_2 {
+	public static void main(String[] args) {
+		int A, B;
+		int sum = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		A = sc.nextInt();
+		B = sc.nextInt();
+		sum = A + B;
+		System.out.print(sum);
+	}
+}
