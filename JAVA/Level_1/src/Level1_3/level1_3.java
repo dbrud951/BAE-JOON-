bad6ed79ce789c7754409eabc8230213f3b8ab74
dbrud951 <dbrud951@hanmail.net>
2018-05-14 20:19:09 +0900

    정수 두 개 입력 받아서 차를 출력하는 프로그램 작성(JAVA)

---
+++ JAVA/Level_1/src/Level1_3/level1_3.java
@@ -0,0 +1,17 @@
+package Level1_3;
+
+import java.util.Scanner;;
+
+public class level1_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int A, B, sub=0;
+		Scanner sc = new Scanner(System.in);
+		A = sc.nextInt();
+		B = sc.nextInt();
+		sub = A - B;
+		System.out.println(sub);
+	}
+
+}
