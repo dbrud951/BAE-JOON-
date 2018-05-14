96a44eab6c2a581547768d581ca42745a55331e6
dbrud951 <dbrud951@hanmail.net>
2018-05-14 20:40:52 +0900

    맞은 문제의 개수와 아이디를 출력하는 프로그램 작성(JAVA)

---
+++ JAVA/Level_1/src/Level1_4/Level1_4.java
@@ -0,0 +1,11 @@
+package Level1_4;
+
+public class Level1_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("3");
+		System.out.println("dbrud951");
+	}
+
+}
